96f2c5ad76dee6fcbb335638aae6a53bc1bc586e
Sujeet's MacBook <sujeetmohanty11@gmail.com>
2024-01-01 21:57:34 +0530

    cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project will help recommend the top3 food items in swiggy home page',
+    author='sujeet',
+    license='',
+)
